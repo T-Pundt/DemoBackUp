491c218b43b1455c24c7b14bf7cbd964417ff37b
Tim <tim.pundt@trojans.dsu.edu>
2024-04-05 19:01:29 -0500

    Inital Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<main>
+
+    <h1>
+        Hello this is the app
+    </h1>
+
+    <button type="button" class="large_button" onclick="window.location.href='sign_In'">Sign In</button>
+    <br><br>
+    <button type="button" class="large_button" onclick="window.location.href='sign_Up'">Sign Up</button>
+
+</main>
+
+
+
