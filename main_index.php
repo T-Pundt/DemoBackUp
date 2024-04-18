1d167c1115cb1c76ffb5dea5c3012085cb8cda54
Brian Gregory <briangregory2011@gmail.com>
2024-04-18 13:18:08 -0400

    CSS testing

--- main_index.php
+++ main_index.php
@@ -1,14 +1,22 @@
+<html>
+    <head>
+        <title>Tracked</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
 <main>
 
-    <h1>
-        Tracked
-    </h1>
+    <div class="center">
+        <h1>
+            Tracked
+        </h1>
 
-    <button type="button" class="large_button" onclick="window.location.href='sign_In'">Sign In</button>
-    <br><br>
-    <button type="button" class="large_button" onclick="window.location.href='sign_Up'">Sign Up</button>
+        <button type="button" class="large_button" onclick="window.location.href='sign_In'">Sign In</button>
+        <br><br>
+        <button type="button" class="large_button" onclick="window.location.href='sign_Up'">Sign Up</button>
+    </div>
 
 </main>
 
 
 
+</html>
