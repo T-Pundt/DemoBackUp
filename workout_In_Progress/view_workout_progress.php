5dbf0eaf38c2f2ee9b5a5231c522e0f41f138a2f
Brian Gregory <briangregory2011@gmail.com>
2024-04-18 13:49:37 -0400

    Workout CSS Linking 2

--- workout_In_Progress/view_workout_progress.php
+++ workout_In_Progress/view_workout_progress.php
@@ -1,10 +1,15 @@
-<main>
+<html>
+    <head>
+        <title>Tracked - Workout In Progress</title>
+        <link rel="stylesheet" href="../css/main.css">
+    </head>
+
+    <main class="center">
     <h1>Workout In Progress</h1>
 
     <?php foreach($workoutExercies as $workoutExercise) : ?>
             <form action="." method="post">
                 <input type="hidden" name="action" value="workout_complete">
-               
                 <p><?php echo htmlspecialchars($workoutExercise['exerciseName'])?></p>
                 <input type="checkbox" required name="completed">Reps: <?php echo htmlspecialchars($workoutExercise['reps'])?> Sets:<?php echo htmlspecialchars($workoutExercise['sets'])?> Weight:<?php echo htmlspecialchars($workoutExercise['weight'])?>
             </form>
@@ -13,4 +18,5 @@
         <form method="post">
         <button type="submit" class="large_button" name="action" value="workout_complete">Complete</button>
         </form>
-</main>
+    </main>
+</html>
