086908251ad0f682b125eaff82b977c6fcadfbcb
Brian Gregory <briangregory2011@gmail.com>
2024-04-18 16:17:31 -0400

    Additional CSS

--- sign_Up/Sign_Up.php
+++ sign_Up/Sign_Up.php
@@ -5,21 +5,21 @@
     </head>
     <main class="center">
 
-        <h1>Sign Up</h1>
-
+        <h1 class="title">Sign Up</h1>
+        <br><br>
         <form action="." method="post" id="aligned">
             <input type="hidden" name="action" value="add_user">
 
             <label>Email</label>
             <input type="text" name="email" required><br>
-
+            <br>
             <label>Username</label>
             <input type="text" name="userName" required><br>
-
+            <br>
             <label>Password</label>
             <input type="text" name="password" required><br>
-            
+            <br><br>
             <label>&nbsp;</label>
-            <input type="submit" value="Create Profile" /><br>
+            <input type="submit" value="Create Profile" class="med_button" /><br>
     </main>
 </html>
