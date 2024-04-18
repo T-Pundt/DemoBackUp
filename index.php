3c1d6af9d21b012a2e901c5c0227069d8bfe023f
Brian Gregory <briangregory2011@gmail.com>
2024-04-18 16:05:20 -0400

    Large Button Text and Look updates

--- index.php
+++ index.php
@@ -7,8 +7,18 @@
             <img src="images/tracked.png"/>
 
             <br>
-            <button type="button" class="large_button" onclick="window.location.href='sign_In'">Sign In</button>
+            <button type="button" class="large_button" onclick="window.location.href='sign_In'">
+            <div class="large_button_text">Sign In</div>
+                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.174805 11.7285H15.1462L8.24505 18.6115L10.0318 20.3325L19.8897 10.5005L10.0328 0.66748L8.30722 2.39048L15.1462 9.27348H0.174805V11.7285Z" fill="#575F6B"/>
+                </svg> 
+            </button>
             <br><br>
-            <button type="button" class="large_button" onclick="window.location.href='sign_Up'">Sign Up</button>
+            <button type="button" class="large_button" onclick="window.location.href='sign_Up'">
+            <div class="large_button_text">Sign Up</div>
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.174805 11.7285H15.1462L8.24505 18.6115L10.0318 20.3325L19.8897 10.5005L10.0328 0.66748L8.30722 2.39048L15.1462 9.27348H0.174805V11.7285Z" fill="#575F6B"/>
+            </svg> 
+        </button>
     </main>
 </html>
